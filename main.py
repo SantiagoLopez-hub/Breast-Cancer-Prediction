3ea19d40ccb44b5829d4eecef06ab4dcdacc75bc
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-03 11:29:13 +0000

    Set training amount

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
 
 
 def classifier():
     # Loading dataset
     data = load_breast_cancer()
+
+    # Set training 80% and testing 20% of data
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.data,
+        data.target,
+        test_size=0.2
+    )
